48109e22e05353d8c02caee3ddd46b8204e5f0b6
Javier <javiteen1250@gmail.com>
2023-06-20 23:34:19 -0300

    rehaciendo la implementacion de servicios

--- src/main/java/com/ar/caiolaboot/service/ObjectSkimpl.java
+++ src/main/java/com/ar/caiolaboot/service/ObjectSkimpl.java
@@ -1,5 +1,44 @@
 package com.ar.caiolaboot.service;
 
-public class ObjectSkimpl {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.ar.caiolaboot.model.ObjectSk;
+import com.ar.caiolaboot.repository.ObjectSkRepo;
+
+@Service
+public class ObjectSkimpl implements ObjectSkService {
+
+	private final ObjectSkRepo repo;
+	
+	public ObjectSkimpl (ObjectSkRepo repo) {
+		this.repo = repo;
+	}
+	
+	@Override
+	public List<ObjectSk> obtenerAll() {
+		return repo.findAll();
+	}
+
+	@Override
+	public ObjectSk buscarId(Long id) {
+		return repo.findById(id).orElse(null);
+	}
+
+	@Override
+	public ObjectSk crear(ObjectSk obj) {
+		return repo.save(obj);
+	}
+
+	@Override
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+
+	@Override
+	public ObjectSk update(ObjectSk obj) {
+		return repo.save(obj);
+	}
+	
 }
