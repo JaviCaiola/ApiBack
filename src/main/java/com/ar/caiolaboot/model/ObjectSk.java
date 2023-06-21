0af19638a2698bc1a4ef3627f589df8c22d425a7
Javier <javiteen1250@gmail.com>
2023-06-20 23:18:22 -0300

    rehaciendo la capa modelo

--- src/main/java/com/ar/caiolaboot/model/ObjectSk.java
+++ src/main/java/com/ar/caiolaboot/model/ObjectSk.java
@@ -1,5 +1,29 @@
 package com.ar.caiolaboot.model;
 
-public class ObjectSk {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name = "Skills")
+@Getter @Setter
+public class ObjectSk {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "nombre")
+	private String name;
+	
+	@Column(name = "url")
+	private String url;
+	
+	
 }
