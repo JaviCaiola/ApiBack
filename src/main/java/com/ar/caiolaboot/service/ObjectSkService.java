c3c73c1661241ebbda99166d01928941bb14e21e
Javier <javiteen1250@gmail.com>
2023-06-20 23:26:57 -0300

    rehaciendo interface de servicio sin implementación

--- src/main/java/com/ar/caiolaboot/service/ObjectSkService.java
+++ src/main/java/com/ar/caiolaboot/service/ObjectSkService.java
@@ -1,5 +1,14 @@
 package com.ar.caiolaboot.service;
 
-public interface ObjectSkService {
+import java.util.List;
 
+import com.ar.caiolaboot.model.ObjectSk;
+
+public interface ObjectSkService {
+	
+	public List<ObjectSk> obtenerAll();
+	public ObjectSk buscarId(Long id);
+	public ObjectSk crear(ObjectSk obj);
+	public void delete(Long id);
+	public ObjectSk update(ObjectSk obj);
 }
