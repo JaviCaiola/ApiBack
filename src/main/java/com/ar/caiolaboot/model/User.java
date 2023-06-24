ec27c07dc6ba16fb74925e91fc433e1bac2f7a45
Javier <javiteen1250@gmail.com>
2023-06-24 17:43:04 -0300

    configurando capa model del usuario

--- src/main/java/com/ar/caiolaboot/model/User.java
+++ src/main/java/com/ar/caiolaboot/model/User.java
@@ -1,5 +1,32 @@
 package com.ar.caiolaboot.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Usuarios")
+@Getter @Setter
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@Column(name = "pass")
+	private String contraseña;
+	
+	public User () {
+		
+	}
+	
 }
